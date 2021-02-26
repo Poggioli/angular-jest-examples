6150d74c50a3ee621d88870640311e7fcea45ae2
unknown <joaopogiolli@gmail.com>
2021-02-25 22:55:24 -0300

    testes do serviço ok

--- src/app/services/vaccine/vaccine.service.spec.ts
+++ src/app/services/vaccine/vaccine.service.spec.ts
@@ -1,16 +1,38 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
+import { environment } from '@env/environment'
 
 import { VaccineService } from './vaccine.service'
 
 describe('VaccineService', () => {
   let service: VaccineService
+  let http: HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    })
     service = TestBed.inject(VaccineService)
+    http = TestBed.inject(HttpTestingController)
   })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy()
+  beforeEach(() => {
+    http.verify()
+  })
+
+  it(`DADO o serviço
+      QUANDO o request for feito
+      ENTÃO deverá ser feito com os valores corretos`, () => {
+    service.getVaccines().subscribe((resp) => {
+      expect(resp).toStrictEqual([])
+    })
+
+    const testRequest = http.expectOne(`${environment.api.baseUrl}/${environment.api.endpoints.vaccines}`)
+
+    testRequest.flush([])
+    expect(testRequest.request.method).toBe('GET')
+    expect(testRequest.request.url).toBe(`${environment.api.baseUrl}/${environment.api.endpoints.vaccines}`)
   })
 })
--- src/app/services/vaccine/vaccine.service.ts
+++ src/app/services/vaccine/vaccine.service.ts
@@ -1,47 +1,8 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { environment } from '@env/environment'
-import { Observable, of } from 'rxjs'
+import { Observable } from 'rxjs'
 import { Vaccine } from 'src/app/interfaces/vaccine'
-
-const httpBody: Vaccine[] = [
-  {
-    name: 'vaccine 1',
-    tip: 'tip 1',
-    doses: 1,
-    date: '05-21-2021'
-  },
-  {
-    name: 'vaccine 2',
-    tip: 'tip 2',
-    doses: 2,
-    date: '07-21-2021'
-  },
-  {
-    name: 'vaccine 3',
-    tip: 'tip 3',
-    doses: 3,
-    date: '05-14-2021'
-  },
-  {
-    name: 'vaccine 4',
-    tip: 'tip 4',
-    doses: 4,
-    date: '05-21-2021'
-  },
-  {
-    name: 'vaccine 5',
-    tip: 'tip 5',
-    doses: 5,
-    date: '10-21-2021'
-  },
-  {
-    name: 'vaccine 6',
-    tip: 'tip 6',
-    doses: 6,
-    date: '12-21-2021'
-  }
-]
 
 @Injectable({
   providedIn: 'root'
@@ -55,8 +16,7 @@ export class VaccineService {
 
   getVaccines(): Observable<Vaccine[]> {
     // ToDo alterar para quando serviço tiver pronto
-    // const url = `${this.baseUrl}/${environment.api.endpoints.vaccines}`
-    // return this._http.get<Vaccine[]>(url)
-    return of(httpBody)
+    const url = `${this.baseUrl}/${environment.api.endpoints.vaccines}`
+    return this._http.get<Vaccine[]>(url)
   }
 }
